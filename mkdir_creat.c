ab2033c4ce6c729594a4f249ca8023be381b1169
Mikaela <mikaela.dean@wsu.edu>
2021-11-08 22:56:46 -0800

    i tried to start it but barely edited it

--- mkdir_creat.c
+++ mkdir_creat.c
@@ -1,5 +1,6 @@
 #include "type.h"
 
+// helper functions and such
 extern int imap, ninodes;
 
 int tst_bit(char *buf, int bit); // in Chapter 11.3.1
@@ -25,6 +26,8 @@ int ialloc(int dev)  // allocate an inode number from inode_bitmap
   return 0;
 }
 
+// meat of the bones here
+
 int mkdir(int dev) {
     // (1). divide pathname into dirname and basename, e.g. pathname=/a/b/c, then dirname=/a/b; basename=c;
 
@@ -37,23 +40,35 @@ int mkdir(int dev) {
     search(pmip, basename); //must return 0;
 
     // (4). call kmkdir(pmip, basename) to create a DIR;
-    // kmkdir() consists of 4 major steps:
-
-    // (4).1. Allocate an INODE and a disk block:
-    int ino = ialloc(dev);
-    int blk = balloc(dev);
-
-    // (4).2. mip = iget(dev, ino) // load INODE into a minode
-    //initialize mip->INODE as a DIR INODE;
-    // mip->INODE.i_block[0] = blk; other i_block[ ] = 0;
-    //mark minode modified (dirty);
-    //iput(mip); // write INODE back to disk
-
-    //(4).3. make data block 0 of INODE to contain . and .. entries;
-    // write to disk block blk.
+    kmkdir(pmip, basename, dev);
     
-    // (4).4. enter_child(pmip, ino, basename); which enters (ino, basename) as a dir_entry to the parent INODE;
-
     // (5). increment parent INODE’s links_count by 1 and mark pmip dirty;
     iput(pmip);
+}
+
+int kmkdir(int pmip, int basename, int dev) {
+  // kmkdir() consists of 4 major steps:
+
+  // (4).1. Allocate an INODE and a disk block:
+  int ino = ialloc(dev);
+  int blk = balloc(dev);
+
+  // (4).2. mip = iget(dev, ino) // load INODE into a minode
+  //initialize mip->INODE as a DIR INODE;
+  // mip->INODE.i_block[0] = blk; other i_block[ ] = 0;
+  //mark minode modified (dirty);
+  //iput(mip); // write INODE back to disk
+
+  //(4).3. make data block 0 of INODE to contain . and .. entries;
+  // write to disk block blk.
+    
+  // (4).4. enter_child(pmip, ino, basename); which enters (ino, basename) as a dir_entry to the parent INODE;
+}
+
+int creat() {
+
+}
+
+int enter_name() {
+
 }
