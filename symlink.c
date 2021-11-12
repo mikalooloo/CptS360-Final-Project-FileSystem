9abb0287516eddbe56faea726afc4d783bae0173
Josephine <josephine.hartmann@wsu.edu>
2021-11-11 23:55:20 -0800

    Edits to symlink function, needs revision.

---
+++ symlink.c
@@ -0,0 +1,51 @@
+//symlink.c --> includes symlink, readLink functions
+
+#include "header.h"
+
+//function symlink creates a symbolic link from new_file to old_file
+//unlike hard links, bc symlink can link to anything, including DIRs, or even files not on the same
+//device.
+//symlink command: symlink old_file new_file
+int symlnk(char *old_file, char *new_file)
+{
+	//initialize buf
+	char buf[1024];
+
+	int dev;
+	int old_ino = getino(old_file);
+
+	//(1). check: old_file must exist and new_file does not exist
+	if(old_ino == 0)
+	{
+		printf("Attention: %s does not exist!\n", old_file);
+		return 0;
+	}
+
+	int n_blk = (old_ino - 1) / 8;
+	int off = (old_ino - 1) % 8;
+	get_block(dev, n_blk, buf);
+
+	INODE *minode = (INODE*)buf + off;
+
+	//check if file type is valid
+	if(!(S_ISDIR(minode->i_mode) || S_ISREG(minode->i_mode)))
+	{
+		printf("Attention: file type invalid!\n");
+		return;
+	}
+
+	//(2). creat new_file: change new_file to LNK type
+	mkdir_creat(new_file);
+
+	int ino = getino(new_file);
+	int blk = (ino -1) / 8;
+	int offset = (ino - 1) % 8;
+	get_block(dev, blk, buf);
+
+	INODE *new_ino = (INODE*)buf + offset;
+
+	//copy name
+	strcpy((char*) new_ino->i_block, new_file);
+
+	put_block(dev, blk, buf);
+}
