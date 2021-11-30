eb7b809819e437507ec74222df948ae534031f16
Josephine <josephine.hartmann@wsu.edu>
2021-11-29 19:37:29 -0800

    added read and cat, my_cat should be fully functional once my_read and my_close are finished

---
+++ read_cat.c
@@ -0,0 +1,55 @@
+//read/cat
+
+#include "header.h"
+
+int read_file()
+{
+	//assume file is opened for RD or RW
+	//ask for a fd and nbytes to read
+	//verify that fd is indeed opened for RD and RW
+	//return (myread(fd, buf, nbytes))
+	
+}
+
+int myread(int fd, char buf[], nbytes)
+{
+	//int count = 0
+	//avil = fileSize - OFT's offset (number of bytes still available in file)
+	//char *cq = buf
+	//
+	//while(nbytes && avil)
+	//	compute LOGICAL BLOCK number and startByte in that block from offset
+	//	...
+}
+
+//cat
+int my_cat(char *pathname)
+{
+	//cat filename:
+	//	char mybuf[1024], dummy = 0; (null char at end of mybuf[]
+	//	int n;
+	//
+	//(1) int fd = open filename for READ
+	//(2) while(n = read(fd, mybuf[1024], 1024))
+	//	{
+	//		mybuf[n] = 0; //as a terminated null string
+	//		//printf("%s", mybuf);
+	//		spit out chars from mybuf[]
+	//	}
+	//(3) close fd
+	
+	char mybuf[1024], dummy = 0;
+	int n;
+
+	char* temp;
+	strcpy(temp, " R");
+
+	int fd = open_file(temp);
+
+	while(n = my_read(fd, mybuf, 1024))
+	{
+		mybuf[n] = 0;
+		printf("%s", mybuf);
+	}
+	my_close(fd);
+}
