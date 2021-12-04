738176179aa4512bda0d88cfc741f1037373e78a
Mikaela <mikaela.dean@wsu.edu>
2021-12-03 18:57:28 -0800

    added new function calls, added my_menu() that prints out all commands and their descriptions

--- main.c
+++ main.c
@@ -37,6 +37,9 @@ int init()
     p->pid = i;
     p->uid = p->gid = 0;
     p->cwd = 0;
+    for (j = 0; j<NFD; j++){
+      p->fd[j] = 0;
+    }
   }
 }
 
@@ -46,6 +49,8 @@ int mount_root()
   printf("mount_root()\n");
   root = iget(dev, 2);
 }
+
+void my_menu();
 
 char *disk = "diskimage";
 int main(int argc, char *argv[ ])
@@ -94,9 +99,9 @@ int main(int argc, char *argv[ ])
   printf("root refCount = %d\n", root->refCount);
 
   // WRTIE code here to create P1 as a USER process
-  
+  printf("\n************************\n");
   while(1){
-    printf("\n[ls|cd|pwd|mkdir|creat|rmdir|link|unlink|symlink|quit]\ninput command :  ");
+    printf("\n[menu|ls|cd|pwd|mkdir|creat|rmdir|link|unlink|symlink|cat|cp|quit]\ninput command :  ");
     fgets(line, 128, stdin);
     line[strlen(line)-1] = 0;
 
@@ -106,8 +111,10 @@ int main(int argc, char *argv[ ])
 
     sscanf(line, "%s %s", cmd, pathname);
     printf("\ncmd=%s pathname=%s\n", cmd, pathname);
-  
-    if (strcmp(cmd, "ls")==0)
+   
+    if (strcmp(cmd, "menu")==0)
+       my_menu();
+    else if (strcmp(cmd, "ls")==0)
        my_ls(pathname);
     else if (strcmp(cmd, "cd")==0)
        my_cd(pathname);
@@ -132,7 +139,30 @@ int main(int argc, char *argv[ ])
     else if (strcmp(cmd, "cp")==0) {
        sscanf(line, "%s %s %s", cmd, pathname, pathname2);
        my_cp(pathname, pathname2); }
+    else if (strcmp(cmd, "open")==0) {
+      int d = -1;
+      sscanf(line, "%s %s %d", cmd, pathname, &d);
+       open_file(pathname, d);
+    }
     else if (strcmp(cmd, "quit")==0)
        quit();
   }
+}
+
+void my_menu() {
+   printf("\n************************\n");
+   printf("\n[menu]\nprints out all possible commands and their descriptions\n");
+   printf("\n[ls (optional directory name)]\nprints files and directories in the current working directory by default in a long list format\n");
+   printf("\n[cd (directory name)]\nchanges the current working directory to the directory listed after this command. use ../ to go to the previous directory\n");
+   printf("\n[pwd]\nprints the current working directory\n");
+   printf("\n[mkdir (new directory name)]\nmakes a new directory\n");
+   printf("\n[creat (new file name)]\ncreates a new file in the current working directory\n");
+   printf("\n[rmdir (directory name to remove)]\nremoves an empty directory\n");
+   printf("\n[link (filename1) (filename2)]\nhard links filename2 to filename1\n");
+   printf("\n[unlink (filename)]\nunlinks filename\n");
+   printf("\n[symlink (name1) (name2)]\nsoft links name2 to name1\n");
+   printf("\n[cat (filename)]\nprints contents of filename\n");
+   printf("\n[cp (filename1) (filename2)]\ncopies filename1 to filename2\n");
+   printf("\n[quit]\nquits application\n");
+   printf("\n************************\n");
 }
