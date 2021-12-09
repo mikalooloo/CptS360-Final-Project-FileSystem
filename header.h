61c3ed186bb608d11be3df384ba287b7deefd990
Mikaela <mikaela.dean@wsu.edu>
2021-12-08 18:20:00 -0800

    added more functions and oorganized commands to be listed by level and usability

--- header.h
+++ header.h
@@ -17,21 +17,34 @@ void separatePathname(char * pathname, char ** dname, char ** bname, char * comm
 int validPathname(char * pathname);
 
 // my_command functions
-int my_menu();
-int my_cd(char * pathname);
+// LEVEL 1
 int my_ls(char * pathname);
+int my_cd(char * pathname);
 int my_pwd(MINODE *wd);
-int quit();
 int my_mkdir(char *pathname);
 int my_creat(char *pathname);
 int my_rmdir(char *pathname);
 int my_link(char *old_file, char *new_file);
 int my_unlink(char *filename);
 int my_symlink(char *old_file, char *new_file);
+// LEVEL 2
 int my_cat(char *pathname);
 int my_cp(char * src, char * dest);
-int my_mv(char * src, char * dest);
-int my_pfd();
+// LEVEL 3
+int my_mount(char * pathname, char * filesys);
+int my_umount(char *filesys);
+// MISC
+int menu();
+int quit();
+// MISC LEVEL 2
+int open_file(char * pathname, int mode);
+int close_file(int fd);
+int pfd();
+int read_file(int fd, int nbytes);
+int write_file(int fd, int nbytes);
+int mymv(char * src, char * dest);
+// MISC LEVEL 3
+
 
 // command helper functions
 int enter_name(MINODE * pip, int ino, char * basename);
@@ -41,9 +54,6 @@ int idalloc(int dev, int ino);
 int bdalloc(int dev, int bno);
 char * rpwd(MINODE *wd, int print);
 int myread(int fd, char buf[], int nbytes);
-
-int open_file(char * pathname, int mode);
-int close_file(int fd);
 
 // debug
 int printMinnodes(int m);
